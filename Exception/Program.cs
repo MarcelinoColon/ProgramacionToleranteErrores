54fb3983f73292935bbe868976eed4c0d233f29a
Marcelino <Marcelino@LTP-CCTV-3>
2025-12-12 14:57:59 -0400

    Add project files.

---
+++ Exception/Program.cs
@@ -0,0 +1,40 @@
+﻿try
+{
+    Console.WriteLine("Escribe un numero: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Escribe otro numero: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    int result = a / b;
+
+    Console.WriteLine("El resultado de la division es: " + result);
+}
+catch(FormatException ex)
+{
+    Console.WriteLine("Debes escribir numeros");
+}
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine("No puedes dividir entre cero" + ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocurrio un error: " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("Programa finalizado");
+}
+
+/*
+Las excepciones son para situaciones inesperadas y poder tomar una decision
+cuando pasen estas situaciiones y que el programa no se cierre.
+
+Las excepciones tienen tipos y se pueden poner tantas excepciones quieras
+clasificar.
+
+sirven para manejar las excepciones individualmente dependiendo su tipo y no solo
+mandar un mensaje generico de error.
+
+*/

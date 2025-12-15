1e6dbcc0b31fe7fed20b8ac3415ec7d31dfbec5d
Marcelino <Marcelino@LTP-CCTV-3>
2025-12-15 16:16:18 -0400

    Modulo de Fluent Validation listo

---
+++ FluentValidationAPI/CustomerValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace FluentValidationAPI
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(customer => customer.Name)
+                .NotEmpty().WithMessage("Nombre es obligatorio")
+                .Length(2, 50).WithMessage("El nombre debe tener entre 2 y 50 caracteres");
+
+            RuleFor(customer => customer.Email)
+                .NotEmpty().WithMessage("Correo electronico es obligatorio")
+                .EmailAddress().WithMessage("Escribir correo electronico valido");
+
+            RuleFor(customer => customer.Balance)
+                .GreaterThanOrEqualTo(0).WithMessage("Saldo no puede ser negativo");
+        }
+    }
+}
---
+++ FluentValidationAPI/Program.cs
@@ -0,0 +1,45 @@
+using FluentValidation;
+using FluentValidationAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddValidatorsFromAssemblyContaining<CustomerValidator>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/customers", async (Customer customer, IValidator<Customer> validator) =>
+{
+    var validationResult = await validator.ValidateAsync(customer);
+
+    if (!validationResult.IsValid)
+    {
+        var errors = validationResult.Errors.Select(e => new
+        {
+            e.PropertyName,
+            e.ErrorMessage
+        });
+
+        return Results.BadRequest(errors);
+    }
+    // Se va a guardar a la base de datos
+
+    return Results.Created($"/customer.{customer.Id}", customer);
+
+});
+
+
+app.Run();
+
+
